2acd9eef71ed65b9f43eb89593968a2297460874
MuhammedMahameed212 <muhamma11@ac.sce.ac.il>
2024-12-19 18:24:32 +0200

    Customer sign up
    added customer sign up

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,40 @@ const User = require('../models/user'); // מודל המשתמש המייצג א
 const router = express.Router();
 
 
+
+// מסלול להרשמה ללקוח CUSTOMER
+router.post('/signup/customer', async (req, res) => {
+    //שולף מהבקשה את הנתונים שם משתמש , אימייל ...  
+    const { username, email, password, gender } = req.body;
+
+    try {
+        // מצפין את הסיסמה
+        const hashedPassword = await bcrypt.hash(password, 12);
+        // יצירת אובייקט משתמש חדש עם הנתונים ומסמן את סוג החשבון
+        const user = new User({ username, email, password: hashedPassword, gender, accountType: 'Customer' });
+        // שומר את המשתמש ב MONGODB
+        await user.save();
+        // מחזיר תשובה ללקוח עם סטטוס 201 הצלחה
+        res.status(201).json({ message: 'Customer account created successfully' });
+    } catch (err) {
+        res.status(500).json({ message: 'Error creating customer account' });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Contractor Sign-Up
 router.post('/signup/contractor', async (req, res) => {
     const { username, email, password, gender } = req.body;
