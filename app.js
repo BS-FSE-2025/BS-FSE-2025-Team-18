a3dfbe1bab4461116bbce2e4be4e8f4fa3c132d9
ismaelbadran <IsmaeBa@ac.sce.ac.il>
2024-12-19 19:56:33 +0200

    homepage, customer prifle managment
    home page for customer and for contractor , and customer profile page managment and edit buttons

--- app.js
+++ app.js
@@ -38,3 +38,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
+
+const userRoutes = require('./routes/auth'); // המסלולים שלך
+app.use('/api', userRoutes); // הוספת הנתיב הבסיסי
