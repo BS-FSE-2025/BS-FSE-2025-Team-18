c91b0d58ae9d900ea1780e0a6bcaa1878e962b9f
BasharAbadi <bashaab4@ac.sce.ac.il>
2024-12-19 20:05:18 +0200

    contractor profile
    contractor profile managment , with edit buttons for the username ,email , password

--- routes/authMiddleware.js
+++ routes/authMiddleware.js
@@ -14,13 +14,5 @@ const verifyToken = (req, res, next) => {
     }
 };
 
-// Middleware לבדיקה האם המשתמש Admin
 
-const verifyAdmin = (req, res, next) => {
-    if (req.user.accountType !== 'Admin') {
-        return res.status(403).json({ message: 'Access denied. Admins only' });
-    }
-    next();
-};
-
-module.exports = { verifyToken, verifyAdmin };
+module.exports = { verifyToken };
